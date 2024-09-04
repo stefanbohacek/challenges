6add4a12ab99e14a90da43a756c4d3f4f0081528
Stefan Bohacek <stefan@stefanbohacek.com>
2024-09-03 20:29:32 -0400

    Fixed issue with results section that have no events no appearing after the search input field was cleared.

--- src/scripts/modules/handleSearch.js
+++ src/scripts/modules/handleSearch.js
@@ -6,39 +6,40 @@ export default (fn) => {
   ];
 
   searchInput.addEventListener("input", (ev) => {
+    [
+      ...document.querySelectorAll(
+        ".result-section.d-none, .result-item.d-none"
+      ),
+    ].forEach((result) => result.classList.remove("d-none"));
+
     if (searchInput.value && searchInput.value.trim().length > 0) {
       window.history.replaceState(
         null,
         null,
         `?search=${encodeURIComponent(searchInput.value)}`
       );
+
+      allResults.forEach((result) => {
+        if (!result.innerText.includes(searchInput.value)) {
+          result.classList.add("d-none");
+        }
+      });
+
+      allResultSections.forEach((section) => {
+        if (
+          [...section.querySelectorAll(".result-item")].filter((result) => {
+            return (
+              !result.classList.contains("d-none") &&
+              !result.innerText.includes("No events.")
+            );
+          }).length === 0
+        ) {
+          section.classList.add("d-none");
+        }
+      });
     } else {
       window.history.replaceState(null, null, "/");
     }
-
-    [
-      ...document.querySelectorAll(
-        ".result-section.d-none, .result-item.d-none"
-      ),
-    ].forEach((result) => result.classList.remove("d-none"));
-    allResults.forEach((result) => {
-      if (!result.innerText.includes(searchInput.value)) {
-        result.classList.add("d-none");
-      }
-    });
-
-    allResultSections.forEach((section) => {
-      if (
-        [...section.querySelectorAll(".result-item")].filter((result) => {
-          return (
-            !result.classList.contains("d-none") &&
-            !result.innerText.includes("No events.")
-          );
-        }).length === 0
-      ) {
-        section.classList.add("d-none");
-      }
-    });
   });
 
   const urlParams = new URLSearchParams(window.location.search);
